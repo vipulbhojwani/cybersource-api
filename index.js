fa04d7f46bd858be3882808a96f80b853a90bd9a
Vipul Bhojwani <vipul.bhojwani@pearson.com>
2020-01-24 15:22:37 +0530

    update

--- index.js
+++ index.js
@@ -1,57 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var path = require('path');
-var PORT = process.env.PORT || 8080;
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var path = require('path');
+// var PORT = process.env.PORT || 8080;
 
 
-var indexRouter = require('./routes/index');
-var cyberSourceRouter = require('./routes/cybersource');
+// var indexRouter = require('./routes/index');
+// var cyberSourceRouter = require('./routes/cybersource');
 
-var cors = require('cors')
-
-
-var app = express();
-
-app.use(cors())
+// var cors = require('cors')
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, '/dist/app')));
-
-app.use('/encode', indexRouter);
-app.use('/cybersource', cyberSourceRouter);
+// app.use(cors())
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// //app.use(express.static(path.join(__dirname, '/dist/app')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(PORT);
+// app.use('/encode', indexRouter);
+// app.use('/cybersource', cyberSourceRouter);
 
 
-module.exports = app;
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+  // next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+  // // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
+// });
+
+// app.listen(PORT);
+
+
+// module.exports = app;
+
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 
