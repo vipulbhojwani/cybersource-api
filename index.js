fcc38cc534bcd9217f143cfb0b814d73f6ca4f24
Vipul Bhojwani <vipul.bhojwani@pearson.com>
2020-01-24 15:46:13 +0530

    updating

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var crypto = require('crypto');
 
 
 var port = process.env.PORT || 8080;
 
 
-// var indexRouter = require('./routes/index');
+var indexRouter = require('routes/index');
 // var cyberSourceRouter = require('./routes/cybersource');
 
 var app = express();
@@ -39,6 +40,37 @@ var app = express();
 //app.use(express.static(path.join(__dirname, '/dist/app')));
 
  app.get('/', (req, res) => res.send('Hello World!'))
+ 
+ 
+ app.get('/encode/:data', function(req, res, next) {
+  
+  // let dataToEncode=req.data;
+
+  console.log(req.params.data);
+  
+  res.send({signature: generateDigest(req.params.data)});
+  
+  //res.send(req.params.data);
+
+  //res.render('index', { title: 'Express' });
+});
+
+
+function generateDigest(payload) {
+
+	var buffer = Buffer.from(payload, 'utf8');
+	
+	const hash = crypto.createHmac('sha256', SECRET_KEY);
+	
+	hash.update(buffer);
+	
+	var digest = hash.digest('base64');
+	
+	return digest;
+}
+
+
+
 
 // app.use('/encode', indexRouter);
 // app.use('/cybersource', cyberSourceRouter);
